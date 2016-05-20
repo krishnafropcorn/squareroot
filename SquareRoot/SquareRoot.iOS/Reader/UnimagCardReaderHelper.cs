af87f6e95c9188fe10909d539e605d344a40d249
krishnafropcorn <krishna.m@fropcorn.com>
2016-05-20 17:48:27 +0530

    Arranged code in proper order

--- SquareRoot/SquareRoot.iOS/Reader/UnimagCardReaderHelper.cs
+++ SquareRoot/SquareRoot.iOS/Reader/UnimagCardReaderHelper.cs
@@ -20,7 +20,7 @@ namespace SquareRoot.iOS.Reader
 
         private Action<string> onCreditCardSwiped;
 
-        public CardDetails CreditCardDetails { get; private set;  }
+        public CardDetails CreditCardDetails { get; private set; }
 
         public void StopListening()
         {
@@ -35,91 +35,116 @@ namespace SquareRoot.iOS.Reader
             this.onCreditCardSwiped = onCreditCardSwiped;
             uniMag.EnableLogging(true);
             reader = new uniMag();
-            reader.SetAutoConnect(false);
-            reader.SetSwipeTimeoutDuration(0);//10
+            reader.Init();
+            reader.SetAutoConnect(true);
+            reader.SetSwipeTimeoutDuration(0);
             reader.SetAutoAdjustVolume(true);
+
+            var center = NSNotificationCenter.DefaultCenter;
+            center.AddObserver(new NSString("uniMagAttachmentNotification"), umDevice_attachment);
+            // uniMag_activate();
         }
 
-        void uniMag_activate(){
-          UmRet currentStatus=  reader.StartUniMag(true);
-            uniMag_registerObservers(true);
-            displayDeviceStatus(currentStatus);
+        //called when uniMag is physically attached
+        private void umDevice_attachment(NSNotification notification)
+        {
+            uniMag_activate();
         }
 
-        void uniMag_deactivate(){
+        void uniMag_activate()
+        {
+            UmRet currentStatus = reader.StartUniMag(true);
+            if (currentStatus == UmRet.Success)
+            {
+                uniMag_registerObservers(true);
+                displayDeviceStatus(currentStatus);
+            }
+        }
+
+        //called when the SDK has read something from the uniMag device
+        // (eg a swipe, a response to a command) and is in the process of decoding it
+        // Use this to provide an early feedback on the UI
+        private void umDataProcessing(NSNotification notification)
+        {
+            UniMagAlert.ShowAlert("Info", "data processing...");
+            try
+            {
+                var data = notification.Object;
+                var data1 = notification.Name;
+                var data2 = notification.UserInfo;
+                var data3 = notification.Object.Description;
+
+                this.onCreditCardSwiped(data.ToString());//This will rise the  StartListening event in the ContentPage
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        //called when SDK received a swipe successfully
+        private void umSwipe_receivedSwipe(NSNotification notification)
+        {
+            UniMagAlert.ShowAlert("Info", "Swipe_receivedSwipe...");
+            try
+            {
+                var data1 = notification.Name;
+                var data2 = notification.UserInfo;
+                var data3 = notification.Object;
+                var data = notification.Object.Description;
+
+                this.onCreditCardSwiped(data2.ToString());//This will rise the  StartListening event in the ContentPage
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        //called when the SDK hasn't received a swipe from the device within a configured
+        // "swipe timeout interval".
+        private void umSwipe_timeout(NSNotification notification)
+        {
+            UniMagAlert.ShowAlert("Info", "Swipe timeout,please try again");
+        }
+
+        void uniMag_deactivate()
+        {
             UmRet currentStatus = UmRet.NotConnected;
             if (reader != null && reader.ConnectionStatus)
             {
-                currentStatus=reader.StartUniMag(false);
+                currentStatus = reader.StartUniMag(false);
             }
             uniMag_registerObservers(false);
             displayDeviceStatus(currentStatus);
         }
 
-
-        void uniMag_registerObservers(bool registerObservers ){
-
-            if (registerObservers == true)
-            {
-                var center = NSNotificationCenter.DefaultCenter;
-                center.AddObserver(new NSString("uniMagAttachmentNotification"), umDevice_attachment);
-                center.AddObserver(new NSString("uniMagDetachmentNotification"), umDevice_detachment);
-                center.AddObserver(new NSString("uniMagInsufficientPowerNotification"), umConnection_lowVolume);
-                center.AddObserver(new NSString("uniMagTimeoutNotification"), umConnection_timeout);
-                center.AddObserver(new NSString("uniMagDidConnectNotification"), umConnection_connected);
-                center.AddObserver(new NSString("uniMagDidDisconnectNotification"), umConnection_disconnected);
-                center.AddObserver(new NSString("uniMagSwipeNotification"), umSwipe_starting);
-                center.AddObserver(new NSString("uniMagTimeoutSwipeNotification"), umSwipe_timeout);
-                center.AddObserver(new NSString("uniMagDataProcessingNotification"), umDataProcessing);
-                center.AddObserver(new NSString("uniMagInvalidSwipeNotification"), umSwipe_invalid);
-                center.AddObserver(new NSString("uniMagDidReceiveDataNotification"), umSwipe_receivedSwipe);
-                center.AddObserver(new NSString("uniMagCmdSendingNotification"), umCommand_starting);
-                center.AddObserver(new NSString("uniMagCommandTimeoutNotification"), umCommand_timeout);
-                center.AddObserver(new NSString("uniMagDidReceiveCmdNotification"), umCommand_receivedResponse);
-                center.AddObserver(new NSString("uniMagSystemMessageNotification"), umSystemMessage);
-            }
-            else
-            {var center = NSNotificationCenter.DefaultCenter;
-                center.RemoveObserver(new NSString("uniMagAttachmentNotification"));
-            }
-        }
-
-        void displayDeviceStatus(UmRet swipeStatus){
-            switch (swipeStatus)
-            {
-                case UmRet.Success: 
-                case UmRet.AlreadyConnected:
-                    IsReaderPlugged = true;
-                    UniMagAlert.ShowAlert("Info","UniMag Connected.");
-                    break;
-                    //handle other cases
-                default : 
-                    StopListening();
-                    UniMagAlert.ShowAlert("Info","UniMag Not Connected,Try again.");
-                    break;
-            }
-        }
-            
-        void displaySwipeStatus(UmRet swipeStatus){
-            switch (swipeStatus)
-            {
-                case UmRet.Success: 
-                    
-                    UniMagAlert.ShowAlert("Info","Swipe Success.");
-                    break;
-                    //handle other cases
-                default : 
-                    CreditCardDetails = null;
-                    UniMagAlert.ShowAlert("Info","Invaild swipe,please try again.");
-                    break;
-            }
-        }
-        //called when uniMag is physically attached
-        private void umDevice_attachment(NSNotification notification)
+        //called when SDK failed to handshake with reader in time. ie, the connection task has timed out
+        private void umConnection_timeout(NSNotification notification)
         {
-            uniMag_activate();
+            //connect again
+            UmRet swipeStatus = this.reader.RequestSwipe;
+            displayDeviceStatus(swipeStatus);
+        }
 
+        ////called when the connection task is successful. SDK's connection state changes to true
+        private void umConnection_connected(NSNotification notification)
+        {
+            UmRet swipeStatus = this.reader.RequestSwipe;
+            displayDeviceStatus(swipeStatus);
+        }
 
+        // wait for a swipe to be made
+        private void umSwipe_starting(NSNotification notification)
+        {
+            UniMagAlert.ShowAlert("Info", "Waiting for card swipe...");
+        }
+
+        //called when umSwipe_invalid
+        private void umSwipe_invalid(NSNotification notification)
+        {
+            //swipe again alert
+            displaySwipeStatus(this.reader.RequestSwipe);
         }
 
         //called when attempting to start the connection task but iDevice's headphone playback volume is too low
@@ -152,56 +177,6 @@ namespace SquareRoot.iOS.Reader
         {
         }
 
-        //called when SDK failed to handshake with reader in time. ie, the connection task has timed out
-        private void umConnection_timeout(NSNotification notification)
-        {
-            //connect again
-            UmRet swipeStatus = this.reader.RequestSwipe;
-            displayDeviceStatus(swipeStatus);
-        }
-
-        ////called when the connection task is successful. SDK's connection state changes to true
-        private void umConnection_connected(NSNotification notification)
-        {
-            UmRet swipeStatus = this.reader.RequestSwipe;
-            displayDeviceStatus(swipeStatus);
-        }
-
-        // wait for a swipe to be made
-        private void umSwipe_starting(NSNotification notification)
-        {
-        }
-
-       //called when umSwipe_invalid
-        private void umSwipe_invalid(NSNotification notification)
-        {
-            //swipe again alert
-            displaySwipeStatus(this.reader.RequestSwipe);
-        }
-
-        //called when the SDK hasn't received a swipe from the device within a configured
-        // "swipe timeout interval".
-        private void umSwipe_timeout(NSNotification notification)
-        {
-            UniMagAlert.ShowAlert("Info","Swipe timeout,please try again");
-        }
-
-        //called when the SDK has read something from the uniMag device
-        // (eg a swipe, a response to a command) and is in the process of decoding it
-        // Use this to provide an early feedback on the UI
-        private void umDataProcessing(NSNotification notification)
-        {
-        }
-           
-        //called when SDK received a swipe successfully
-        private void umSwipe_receivedSwipe(NSNotification notification)
-        {
-            //NSData *data = [notification object];
-            //NSLog(@"[[ %@ ]]",[data description]);
-            string data = notification.Object.Description;
-            this.onCreditCardSwiped(data);//This will rise the  StartListening event in the ContentPage
-        }
-
         //called when SDK successfully starts to send a command. SDK starts the command
         // task
         private void umCommand_starting(NSNotification notification)
@@ -219,12 +194,87 @@ namespace SquareRoot.iOS.Reader
         {
         }
 
-        void determineNextStep(){
+        private void uniMag_registerObservers(bool registerObservers)
+        {
+            if (registerObservers)
+            {
+                var center = NSNotificationCenter.DefaultCenter;
+                //center.AddObserver(new NSString("uniMagAttachmentNotification"), umDevice_attachment);
+                center.AddObserver(new NSString("uniMagDetachmentNotification"), umDevice_detachment);
+                center.AddObserver(new NSString("uniMagInsufficientPowerNotification"), umConnection_lowVolume);
+                center.AddObserver(new NSString("uniMagTimeoutNotification"), umConnection_timeout);
+                center.AddObserver(new NSString("uniMagDidConnectNotification"), umConnection_connected);
+                center.AddObserver(new NSString("uniMagDidDisconnectNotification"), umConnection_disconnected);
+                center.AddObserver(new NSString("uniMagSwipeNotification"), umSwipe_starting);
+                center.AddObserver(new NSString("uniMagTimeoutSwipeNotification"), umSwipe_timeout);
+                center.AddObserver(new NSString("uniMagDataProcessingNotification"), umDataProcessing);
+                center.AddObserver(new NSString("uniMagInvalidSwipeNotification"), umSwipe_invalid);
+                center.AddObserver(new NSString("uniMagDidReceiveDataNotification"), umSwipe_receivedSwipe);
+                center.AddObserver(new NSString("uniMagCmdSendingNotification"), umCommand_starting);
+                center.AddObserver(new NSString("uniMagCommandTimeoutNotification"), umCommand_timeout);
+                center.AddObserver(new NSString("uniMagDidReceiveCmdNotification"), umCommand_receivedResponse);
+                center.AddObserver(new NSString("uniMagSystemMessageNotification"), umSystemMessage);
+            }
+            else
+            {
+                var center = NSNotificationCenter.DefaultCenter;
+                center.RemoveObserver(new NSString("uniMagAttachmentNotification"));
+                center.RemoveObserver(new NSString("uniMagDetachmentNotification"));
+                center.RemoveObserver(new NSString("uniMagInsufficientPowerNotification"));
+                center.RemoveObserver(new NSString("uniMagTimeoutNotification"));
+                center.RemoveObserver(new NSString("uniMagDidConnectNotification"));
+                center.RemoveObserver(new NSString("uniMagDidDisconnectNotification"));
+                center.RemoveObserver(new NSString("uniMagSwipeNotification"));
+                center.RemoveObserver(new NSString("uniMagTimeoutSwipeNotification"));
+                center.RemoveObserver(new NSString("uniMagDataProcessingNotification"));
+                center.RemoveObserver(new NSString("uniMagInvalidSwipeNotification"));
+                center.RemoveObserver(new NSString("uniMagDidReceiveDataNotification"));
+                center.RemoveObserver(new NSString("uniMagCmdSendingNotification"));
+                center.RemoveObserver(new NSString("uniMagCommandTimeoutNotification"));
+                center.RemoveObserver(new NSString("uniMagDidReceiveCmdNotification"));
+                center.RemoveObserver(new NSString("uniMagSystemMessageNotification"));
+            }
+        }
+
+        void displayDeviceStatus(UmRet swipeStatus)
+        {
+            switch (swipeStatus)
+            {
+                case UmRet.Success: 
+                case UmRet.AlreadyConnected:
+                    IsReaderPlugged = true;
+                    UniMagAlert.ShowAlert("Info", "Reader is connected.");
+                    break;
+                    //handle other cases
+                default : 
+                    StopListening();
+                    UniMagAlert.ShowAlert("Info", "Reader not connected, please try reinserting the reader firmly.");
+                    break;
+            }
+        }
+
+        void displaySwipeStatus(UmRet swipeStatus)
+        {
+            switch (swipeStatus)
+            {
+                case UmRet.Success: 
+                    UniMagAlert.ShowAlert("Info", "Swipe Success.");
+                    break;
+                    //handle other cases
+                default : 
+                    CreditCardDetails = null;
+                    UniMagAlert.ShowAlert("Info", "Invaild swipe, please try again.");
+                    break;
+            }
+        }
+
+        void determineNextStep()
+        {
             // this is where we shuttle the data
         }
         //Our methods
-//        private void onCardDetailsReceived(string cardDetails)
-//        {
-//        }
+        //        private void onCardDetailsReceived(string cardDetails)
+        //        {
+        //        }
     }
 }
---
+++ SquareRoot/SquareRoot/ViewModels/SwipeCardViewModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Microsoft.Practices.Unity;
+using Payment;
+using Common;
+
+namespace SquareRoot
+{
+    public class SwipeCardViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        public ICommand ChargeCommand { get; set; }
+
+        public bool HasValidInput 
+        {
+            get 
+            {
+                return !string.IsNullOrEmpty(_cvv.ToString()) && !string.IsNullOrEmpty(_amount.ToString()); 
+            }
+        }
+
+        private int _cvv;
+
+        private int _amount;
+
+        public int CVV
+        {
+            get { return _cvv; }
+            set
+            {
+                if (value != _cvv)
+                {
+                    _cvv = value;
+                    OnPropertyChanged("CVV");
+                    OnPropertyChanged("HasValidInput");
+                }
+            }
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set
+            {
+                if (value != _amount)
+                {
+                    _amount = value;
+                    OnPropertyChanged("Amount");
+                    OnPropertyChanged("HasValidInput");
+                }
+            }
+        }
+
+        public SwipeCardViewModel()
+        {
+            CVV = null;
+
+            Amount = null;
+
+            ChargeCommand = new Command(Charge);
+
+        }
+
+        private async void Charge()
+        {
+//                var paymentService = UnityProvider.Container.Resolve<IPaymentService>();
+//                var result = await paymentService.ChargeCard(new CardDetails()
+//                    {
+//                        CreditCardNumber = "4000000000000077",
+//                        CardExpiryMonth = 04,
+//                        CardExpiryYear = 2018,
+//                        CVV = TxtCCV.Text
+//                    }, Convert.ToInt16(TxtAmonut.Text));
+//
+//                if (result.IsSuccessFull)
+//                {
+//                    await DisplayAlert("Payment Done", "YoooHooo! We just charged $1000 on your card", "OK");
+//                    await DisplayAlert("Just Kidding", "We are running on test account so nothing was charged :-)", "OK");
+//                }
+//                else
+//                {
+//                    await DisplayAlert("Payment Failed", "Because: " + result.FailureMessage, "OK");
+//                }
+        }
+
+    }
+}
+
