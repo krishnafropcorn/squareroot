2c9df67951020ceaaccbb8e4146176800aba9813
kartikbb <Kartik Bansal>
2016-05-23 20:43:34 +0530

    Forgot changes in CardDetails

--- Common/CardDetails.cs
+++ Common/CardDetails.cs
@@ -1,36 +1,33 @@
-﻿namespace Common
+﻿using System;
+
+namespace Common
 {
-    public class CardDetails
-    {
-        public int CardExpiryMonth
-        {
-            get;
-            set;
-        }
+	public class CardDetails
+	{
+		public int CardExpiryMonth {
+			get;
+			set;
+		}
 
-        public int CardExpiryYear
-        {
-            get;
-            set;
-        }
+		public int CardExpiryYear {
+			get;
+			set;
+		}
 
-        public string CardFirstName
-        {
-            get;
-            set;
-        }
+		public string CardFirstName {
+			get;
+			set;
+		}
 
-        public string CardLastName
-        {
-            get;
-            set;
-        }
+		public string CardLastName {
+			get;
+			set;
+		}
 
-        public string CardRawNameCaptured
-        {
-            get;
-            set;
-        }
+		public string CardRawNameCaptured {
+			get;
+			set;
+		}
 
 		public string CreditCardNumber {
 			get;
@@ -46,42 +43,62 @@
 			get;
 			set;
 		}
-    }
 
-	public class CardToken {
+		public CardDetails (string data)
+		{
+			string tempData = data.ToString ();
+			string[] dataSubstrings = tempData.Split ('^');
+			if (dataSubstrings.Length > 2) {
+				string[] nameValues = dataSubstrings [1].Split ('/');
+				if (nameValues.Length >= 2) {
+					CardLastName = nameValues [0].Trim ();
+					CardFirstName = nameValues [1].Trim ();
+				}
 
-		public string ProviderTokenId
-        {
-            get;
-            set;
-        }
+				string[] cardDetails = dataSubstrings [2].Split (';');
+				if (cardDetails.Length >= 2) {
+					string[] cardNumbers = cardDetails [1].Split ('=');
+					if (cardNumbers.Length >= 1)
+						CreditCardNumber = cardNumbers [0].Trim ();
 
-        public string ProviderCardFingerprint
-        {
-            get;
-            set;
-        }
-
-        public string ProviderCardId
-        {
-            get;
-            set;
-        }
-
-        public string CardBrand
-        {
-            get;
-            set;
-        }
-
-        public string CardLast4
-        {
-            get;
-            set;
-        }
+					CardExpiryYear = Convert.ToInt32 (cardDetails [0].Substring (0, 2));
+					CardExpiryMonth = Convert.ToInt32 (cardDetails [0].Substring (2, 2));
+				}
+			}
+		}
 	}
 
-	public class ChargeCardResponse {
+	public class CardToken
+	{
+
+		public string ProviderTokenId {
+			get;
+			set;
+		}
+
+		public string ProviderCardFingerprint {
+			get;
+			set;
+		}
+
+		public string ProviderCardId {
+			get;
+			set;
+		}
+
+		public string CardBrand {
+			get;
+			set;
+		}
+
+		public string CardLast4 {
+			get;
+			set;
+		}
+	}
+
+	public class ChargeCardResponse
+	{
 		public bool IsSuccessFull {
 			get;
 			set;
